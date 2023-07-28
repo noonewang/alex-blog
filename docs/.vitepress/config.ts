0fa36f85257845d324a3815349044824be48a4cc
alex <alexyuan0818@gmail.com>
2023-07-28 17:37:38 +0800

    format config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitepress'
+import {defineConfig} from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -12,43 +12,46 @@ export default defineConfig({
       { text: 'Code', link: '/blog/know_nothing' }
     ],
 
-    // sidebar: [
-    //   {
-    //     text: 'Examples',
-    //     items: [
-    //       { text: 'Markdown Examples', link: '/blog/markdown-examples' },
-    //       { text: 'Runtime API Examples', link: '/blog/api-examples' }
-    //     ]
-    //   }
-    // ],
+        // sidebar: [
+        //   {
+        //     text: 'Examples',
+        //     items: [
+        //       { text: 'Markdown Examples', link: '/blog/markdown-examples' },
+        //       { text: 'Runtime API Examples', link: '/blog/api-examples' }
+        //     ]
+        //   }
+        // ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/noonewang' }
-    ],
+        socialLinks: [
+            {icon: 'github', link: 'https://github.com/noonewang'}
+        ],
 
-    search: {
-      provider: 'local'
-    }
-  },
-  markdown: {
-    lineNumbers: true
-  },
-  head: [
-      [
-        'link',
-        {rel: 'icon', href: '/golang.png'}
-      ],
-      [
-        'link',
-        {rel: 'preconnect', href: 'https://fonts.googleapis.com'}
-      ],
-      [
-        'link',
-        {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ''},
-      ],
-      [
-        'link',
-        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap'},
-      ],
-  ]
+        search: {
+            provider: 'local'
+        }
+    },
+    markdown: {
+        lineNumbers: true
+    },
+    head: [
+        [
+            'link',
+            {rel: 'icon', href: '/golang.png'}
+        ],
+        [
+            'link',
+            {rel: 'preconnect', href: 'https://fonts.googleapis.com'}
+        ],
+        [
+            'link',
+            {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ''},
+        ],
+        [
+            'link',
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap'
+            },
+        ],
+    ]
 })
