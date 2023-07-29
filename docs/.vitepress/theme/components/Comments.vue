9fa05b0925f147f8c77acc011800a8cccf6e9a06
alex <alexyuan0818@gmail.com>
2023-07-29 10:21:08 +0800

    add github discussion

---
+++ docs/.vitepress/theme/components/Comments.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {useData} from 'vitepress'
+
+const {title} = useData()
+</script>
+
+<template>
+  <div class="comments">
+    <component
+        :is="'script'"
+        :key="title"
+        src="https://giscus.app/client.js"
+        data-repo="noonewang/alex-blog"
+        data-repo-id="R_kgDOJ-eT6w"
+        data-category="Announcements"
+        data-category-id="DIC_kwDOJ-eT684CYNEZ"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="preferred_color_scheme"
+        data-lang="zh-CN"
+        crossorigin="anonymous"
+        async
+    />
+  </div>
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,15 +1,18 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
 import Theme from 'vitepress/theme'
+// @ts-ignore
+import Layout from './components/Layout.vue'
 import './style.css'
 
 export default {
   extends: Theme,
-  Layout: () => {
-    return h(Theme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+  // Layout: () => {
+  //   return h(Theme.Layout, null, {
+  //     // https://vitepress.dev/guide/extending-default-theme#layout-slots
+  //   })
+  // },
+  Layout: Layout,
   enhanceApp({ app, router, siteData }) {
     // ...
   }
